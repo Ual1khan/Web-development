02e489ef411b358e20f4beb21ea60df3593de5e9
Ual1khan <ual1han999@gmail.com>
2020-02-27 22:47:05 +0600

    week4 and week5 added

---
+++ week5/lab5/src/app/category.ts
@@ -0,0 +1,11 @@
+export interface Category{
+    id:number;
+    name:string;
+}
+
+export const CATEGORY: Category[] = [
+{id:1, name:"Toys"},
+{id:2, name:"Technologies"},
+{id:3, name:"Accessories"},
+{id:4, name:"For home"}
+];
